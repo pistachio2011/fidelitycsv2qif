89ba3cd9ca855316c9145859927a862f30f46fda
pistachio_2011 <test@github.com>
2017-10-29 19:54:32 -0400

    init commit

---
+++ src/java/investment/FidelityCSV2QIF.java
@@ -0,0 +1,152 @@
+package investment;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+import au.com.bytecode.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
+
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Copyright 2005 Bytecode Pty Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+public class FidelityCSV2QIF {
+	private String inputCSV = null;
+	private String outputQif = null;
+	private String stagingCSV = null;
+	
+	static Map<String, String> HEADER_MAP = new HashMap<String, String>();
+	static {
+		HEADER_MAP.put("Run Date", "tradeDate");
+		HEADER_MAP.put("Action", "action");
+		HEADER_MAP.put("Security Description", "securityDesc");
+		HEADER_MAP.put("Price ($)", "price");
+		HEADER_MAP.put("Symbol", "symbol");
+		HEADER_MAP.put("Security Type", "securityType");
+		HEADER_MAP.put("Quantity", "quantity");
+		HEADER_MAP.put("Commission ($)", "commission");
+		HEADER_MAP.put("Fees ($)", "fees");
+		HEADER_MAP.put("Accrued Interest ($)", "accruedInterest");
+		HEADER_MAP.put("Amount", "amount");
+		HEADER_MAP.put("Amount ($)", "amount");
+		HEADER_MAP.put("Settlement Date", "settlementDate");
+	}
+
+	public void cleanCSVfile() throws IOException {
+		try (PrintWriter  writer = new PrintWriter(Files.newBufferedWriter(Paths.get(stagingCSV)))) {
+			try (Stream<String> stream = Files.lines(Paths.get(inputCSV))) {
+			    stream
+			         .map(String::trim)
+			         .filter(line->!line.isEmpty())
+			         .forEach(line -> writer.println(line))
+			         ;
+			   
+			    stream.close();
+			}
+			writer.flush();
+		    writer.close();
+		}
+		
+	}
+	
+	public FidelityCSV2QIF(String inputCSVName) {
+		super();
+		inputCSV = inputCSVName;
+		this.stagingCSV = this.inputCSV + ".staging.csv";
+		this.outputQif = this.inputCSV + ".qif";
+	}
+
+	public void createQIF(String header) throws IOException {
+		cleanCSVfile();
+		CSVReader reader = new CSVReader(new FileReader(this.stagingCSV));
+		List<InvestmentTransaction> transactions = parseCSV(reader);
+		boolean needheader = true;
+		File outputFile = new File(this.outputQif);
+		if (outputFile.length() > 0) {
+			needheader = false;
+		}
+		
+		PrintWriter pw = new PrintWriter(new FileWriter(outputFile,true), true);
+		if (needheader){
+			pw.println(header);
+		}
+		int actualTransNo = 0;
+		for (int i = 0; i < transactions.size(); i++) {
+			InvestmentTransaction t = transactions.get(i);
+			if (!t.isIgnorable()) {
+				if ("FEE CHARGED".equals(t.getAction())) {
+					t.setCategory("Bank Charges:Fee Charged");
+				} else if ("FOREIGN TAX PAID".equals(t.getAction())) {
+					t.setCategory("Taxes:FOREIGN TAX PAID");
+				}
+				pw.println(t.toQIFString());
+				actualTransNo++;
+			}
+		}
+		pw.close();
+		System.out.println(actualTransNo + " transaction generated.");
+	}
+	
+
+
+
+	public static void main(String[] args) throws IOException {
+		FidelityCSV2QIF converter = new FidelityCSV2QIF(args[0]);
+   		converter.createQIF("!Type:Invst");
+	}
+
+	public static List<InvestmentTransaction> parseCSV(CSVReader reader) {
+
+		HeaderColumnNameTranslateMappingStrategy<InvestmentTransaction> strat = new HeaderColumnNameTranslateMappingStrategy<InvestmentTransaction>();
+		strat.setType(InvestmentTransaction.class);
+
+		strat.setColumnMapping(HEADER_MAP);
+
+		CsvToBean<InvestmentTransaction> csv = new CsvToBean<InvestmentTransaction>();
+
+		List<InvestmentTransaction> list = csv.parse(strat, reader);
+		return list;
+	}
+	
+	public String getInputCSV() {
+		return inputCSV;
+	}
+
+	public void setInputCSV(String inputCSV) {
+		this.inputCSV = inputCSV;
+	}
+
+	public String getOutputQif() {
+		return outputQif;
+	}
+
+	public void setOutputQif(String outputQif) {
+		this.outputQif = outputQif;
+	}
+
+	public String getStagingCSV() {
+		return stagingCSV;
+	}
+
+	public void setStagingCSV(String stagingCSV) {
+		this.stagingCSV = stagingCSV;
+	}
+
+
+}
---
+++ src/java/investment/InvestmentTransaction.java
@@ -0,0 +1,266 @@
+package investment;
+import java.util.*;
+
+public class InvestmentTransaction {
+	private String tradeDate = null;
+	private String action = null;
+	private String securityDesc = null;
+	private String price = null;
+	private String symbol = null;
+	private String securityType = null;
+	private String quantity = null;
+	private String commission = null;
+	private String fees = null;
+	private String accruedInterest = null;
+	private String amount = null;
+	private String settlementDate = null;
+	private String category = null;
+	
+	private String clean(String inStr){
+		if (inStr == null) {
+			return "";
+		}
+		return inStr.trim();
+	}
+	public String getTradeDate() {
+		return clean(tradeDate);
+	}
+	public void setTradeDate(String tradeDate) {
+		this.tradeDate = tradeDate;
+	}
+	public String getMoneyAction() {
+		return ACTION_MAP.get(getAction());
+	}
+	
+	public String getAction() {
+		String act = clean(action);
+		if (act.startsWith("YOU BOUGHT")) {
+			return "YOU BOUGHT";
+		}
+		if (act.startsWith("YOU SOLD")) {
+			return "YOU SOLD";
+		}
+		int index = act.indexOf(" as of");
+		if (index > 0) {
+			return act.substring(0, index);
+		}
+		return act;
+	}
+	
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getSecurityDesc() {
+		return clean(securityDesc);
+	}
+	public void setSecurityDesc(String securityDesc) {
+		this.securityDesc = securityDesc;
+	}
+	public String getPrice() {
+		return clean(price);
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getSymbol() {
+		return clean(symbol);
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getSecurityType() {
+		return clean(securityType);
+	}
+	public void setSecurityType(String securityType) {
+		this.securityType = securityType;
+	}
+	public String getQuantity() {
+		if (this.quantity == null || quantity.length() == 0) {
+			return "0";
+		}
+		if (this.quantity.trim().startsWith("-")){
+			return quantity.trim().substring(1);
+		}
+		return clean(quantity);
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getCommission() {
+		return clean(commission);
+	}
+	public void setCommission(String commission) {
+		this.commission = commission;
+	}
+	public String getFees() {
+		return clean(fees);
+	}
+	public void setFees(String fees) {
+		this.fees = fees;
+	}
+	public String getAccruedInterest() {
+		return clean(accruedInterest);
+	}
+	public void setAccruedInterest(String accruedInterest) {
+		this.accruedInterest = accruedInterest;
+	}
+	public String getAmount() {
+		return clean(amount);
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getSettlementDate() {
+		return clean(settlementDate);
+	}
+	public void setSettlementDate(String settlementDate) {
+		this.settlementDate = settlementDate;
+	}
+	
+   static Map<String, String> ACTION_MAP = new HashMap<String, String>();	
+	static {
+		ACTION_MAP.put("YOU SOLD", "Sell");
+		ACTION_MAP.put("IN LIEU OF FRX SHARE", "Sell");
+	
+		ACTION_MAP.put("REINVESTMENT", "ReinvDiv");
+		ACTION_MAP.put("DIVIDEND RECEIVED", "Div");
+		ACTION_MAP.put("YOU BOUGHT", "Buy");
+		ACTION_MAP.put("SHORT-TERM CAP GAIN", "CGShort");
+		ACTION_MAP.put("FOREIGN TAX PAID", "MiscExp");
+		ACTION_MAP.put("FEE CHARGED", "MiscExp");
+		ACTION_MAP.put("Buy", "Buy");
+		ACTION_MAP.put("Sell", "Sell");
+		ACTION_MAP.put("Shares In", "ReinvDiv");
+		ACTION_MAP.put("Add Shares", "ShrsIn");
+		ACTION_MAP.put("REDEMPTION PAYOUT", "Redeem CD/Bond");
+		ACTION_MAP.put("INTEREST", "Interest");
+		ACTION_MAP.put("INTEREST EARNED", "Interest");
+	}
+	
+	static Set<String> IGNORABLE_ACTION = new HashSet<String>();
+	static {
+		IGNORABLE_ACTION.add("");
+		IGNORABLE_ACTION.add("PURCHASE INTO CORE ACCOUNT");
+		IGNORABLE_ACTION.add("REDEMPTION FROM CORE ACCOUNT");
+		IGNORABLE_ACTION.add("JOURNALED");
+	}
+	
+	public boolean isIgnorable(){
+	    if (this.action == null) {
+	    	return true;
+	    }
+	    if ("FDRXX".equals(getSymbol()) && "REINVESTMENT".equals(getAction())) {
+	    	return true;
+	    }
+	    if (IGNORABLE_ACTION.contains(getAction())) {
+	    	return true;
+	    }
+	    return false;
+	}
+	
+	public String toString(){
+		StringBuffer sb = new StringBuffer();
+		sb.append(this.getTradeDate());
+		sb.append(this.getMoneyAction());
+		sb.append(this.symbol);
+		sb.append(this.commission);
+		sb.append(this.quantity);
+		sb.append(this.price);
+		sb.append(this.amount);
+		return sb.toString();
+	}
+	
+	public String toQIFString() {
+		if (this.getSymbol() == null) {
+			return this.toQIFBankString();
+		} else {
+			return this.toQIFInvestmentString();
+		}
+	}
+	
+	public String toQIFInvestmentString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("D");
+		sb.append(this.getTradeDate());
+		sb.append("\nM");
+		sb.append(this.getAction());
+		sb.append("\nT");
+		sb.append(this.getAmount());
+		sb.append("\nN");
+		sb.append(this.getMoneyAction());
+		sb.append("\nY");
+		sb.append(this.getSecurityDesc());
+		sb.append("\nI");
+		sb.append(this.getPrice());
+		sb.append("\nQ");
+		sb.append(this.getQuantity());
+		sb.append("\nO");
+		sb.append(this.getCommission());
+		sb.append("\nL");
+		sb.append(this.getCategory());
+		sb.append("\n^");
+		return sb.toString();	
+	}
+		
+	public String toQIFBankString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("D");
+		sb.append(this.getTradeDate());
+		sb.append("\nM");
+		sb.append(this.getAction());
+		sb.append("\nT");
+		sb.append(this.getAmount());
+		sb.append("\nP");
+		sb.append(this.getSecurityDesc());
+		sb.append("\nL");
+		sb.append("\n^");
+		return sb.toString();
+		
+		
+//	       // Standard fields
+//        typemap.put("D", "date");
+//        typemap.put("T", "amount");
+//        typemap.put("P", "description");
+//        typemap.put("C", "cleared"); 
+//        typemap.put("N", "checknumber");
+//        typemap.put("A", "address");
+//        typemap.put("M", "memo");
+//        typemap.put("L", "category");
+//        typemap.put("F", "reimbursable");
+//        
+        /*
+         * Going to have to do subclasses for Splits 
+         * and Investments because many of the fields are 
+         * re-defined. I think QIFTransaction will wind
+         * up just being a base class, and all of this
+         * work moved to subclasses. 
+         * 
+         */
+        
+        
+        /*
+        // Split / Investment common fields
+        typemap.put("$", "amount");
+        
+        // Splits
+        typemap.put("S", "splitcategory");
+        typemap.put("E", "splitmemo");
+        typemap.put("%", "splitpercent");
+        
+        // Investment types
+        
+        typemap.put("N", "action"); // Buy, sell...
+        typemap.put("Y", "securityname"); 
+        typemap.put("I", "price");
+        typemap.put("Q", "quantity");
+        typemap.put("O", "commission");
+        */
+
+	}
+	public String getCategory() {
+		return clean(category);
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
